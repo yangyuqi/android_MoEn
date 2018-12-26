2beaf24b715d1e12aff2ac5780c64670a6f6c42d
zoujingkang <879360785@qq.com>
2018-12-26 17:41:27 +0800

    增加新布局及逻辑12

---
+++ app/src/main/java/com/youzheng/zhejiang/robertmoog/Store/activity/ProfessionalCustomerOrderActivity.java
@@ -0,0 +1,118 @@
+package com.youzheng.zhejiang.robertmoog.Store.activity;
+
+import android.content.res.Resources;
+import android.os.Bundle;
+import android.support.design.widget.TabLayout;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.view.ViewPager;
+import android.util.TypedValue;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
+import android.widget.RelativeLayout;
+import android.widget.TextView;
+import com.youzheng.zhejiang.robertmoog.Base.BaseActivity;
+import com.youzheng.zhejiang.robertmoog.R;
+import com.youzheng.zhejiang.robertmoog.Store.adapter.ProfessionalCustomerPagerAdapter;
+import com.youzheng.zhejiang.robertmoog.Store.fragment.CustomerOrderFragment;
+
+import java.lang.reflect.Field;
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * 专业客户订单界面
+ */
+public class ProfessionalCustomerOrderActivity extends BaseActivity implements View.OnClickListener {
+
+    private ImageView btnBack;
+    /**  */
+    private TextView textHeadTitle;
+    /**  */
+    private TextView textHeadNext;
+    private ImageView iv_next;
+    private RelativeLayout layout_header;
+    private TabLayout tab;
+    private ViewPager pager;
+    private List<Fragment> list=new ArrayList<>();
+    private ProfessionalCustomerPagerAdapter pagerAdapter;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_professional_customer_order);
+        initView();
+    }
+
+    private void initView() {
+        btnBack = (ImageView) findViewById(R.id.btnBack);
+        btnBack.setOnClickListener(this);
+        textHeadTitle = (TextView) findViewById(R.id.textHeadTitle);
+        textHeadTitle.setText("订单列表");
+        textHeadNext = (TextView) findViewById(R.id.textHeadNext);
+        iv_next = (ImageView) findViewById(R.id.iv_next);
+        layout_header = (RelativeLayout) findViewById(R.id.layout_header);
+        tab = (TabLayout) findViewById(R.id.tab);
+        pager = (ViewPager) findViewById(R.id.pager);
+        initPager();
+    }
+
+    private void initPager() {
+        list.add(new CustomerOrderFragment());
+        list.add(new CustomerOrderFragment());
+
+
+        FragmentManager fm = getSupportFragmentManager();
+        pagerAdapter = new ProfessionalCustomerPagerAdapter(fm, list);
+        pager.setAdapter(pagerAdapter);
+        pager.setOffscreenPageLimit(2);
+
+        tab.setupWithViewPager(pager);
+        //默认选中
+        tab.getTabAt(0).select();
+    }
+
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()) {
+            default:
+                break;
+            case R.id.btnBack:
+                finish();
+                break;
+        }
+    }
+
+    public void setIndicator(TabLayout tabs, int leftDip, int rightDip) {
+        Class<?> tabLayout = tabs.getClass();
+        Field tabStrip = null;
+        try {
+            tabStrip = tabLayout.getDeclaredField("mTabStrip");
+        } catch (NoSuchFieldException e) {
+            e.printStackTrace();
+        }
+
+        tabStrip.setAccessible(true);
+        LinearLayout llTab = null;
+        try {
+            llTab = (LinearLayout) tabStrip.get(tabs);
+        } catch (IllegalAccessException e) {
+            e.printStackTrace();
+        }
+
+        int left = (int) TypedValue.applyDimension(TypedValue.COMPLEX_UNIT_DIP, leftDip, Resources.getSystem().getDisplayMetrics());
+        int right = (int) TypedValue.applyDimension(TypedValue.COMPLEX_UNIT_DIP, rightDip, Resources.getSystem().getDisplayMetrics());
+
+        for (int i = 0; i < llTab.getChildCount(); i++) {
+            View child = llTab.getChildAt(i);
+            child.setPadding(0, 0, 0, 0);
+            LinearLayout.LayoutParams params = new LinearLayout.LayoutParams(0, LinearLayout.LayoutParams.MATCH_PARENT, 1);
+            params.leftMargin = left;
+            params.rightMargin = right;
+            child.setLayoutParams(params);
+            child.invalidate();
+        }
+    }
+}
