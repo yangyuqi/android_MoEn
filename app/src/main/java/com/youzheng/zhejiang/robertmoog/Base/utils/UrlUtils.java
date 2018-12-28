4b872745b4dbde75c1da8a8dc131748c93f32b13
yuqi.yang <yuqi.yang@ughen.com>
2018-12-28 16:05:46 +0800

    urlUtils

--- app/src/main/java/com/youzheng/zhejiang/robertmoog/Base/utils/UrlUtils.java
+++ app/src/main/java/com/youzheng/zhejiang/robertmoog/Base/utils/UrlUtils.java
@@ -6,11 +6,15 @@ public class UrlUtils {
     public static String REGISTER_USER = BASE_URL+"/v1/app/customer/registerCustomer";
     public static String SEND_CODE = BASE_URL+"/v1/app/user/getCheckCode";
     public static String GET_USER_ONFO = BASE_URL+"/v1/app/user/getUserConfig";
-    public static String CUSTOMER_ONTENT = BASE_URL+"/v1/app/customer/shopCustomerIntent";
+    public static String CUSTOMER_SHOP = BASE_URL+"/v1/app/customer/shopCustomerIntent";
     public static String SHOP_PERSONAL = BASE_URL+"/v1/app/customer/shopPersonal";
     public static String NOT_LABEEL = BASE_URL+"/v1/app/customer/notLabel";
     public static String SHOP_SCVAN = BASE_URL+"/v1/app/customer/shopQRCode";
     public static String ATTENTION_GOODS_LIST = BASE_URL+"/v1/app/customer/intentProductList";
     public static String HOME_INFO = BASE_URL+"/v1/app/user/getHomePage";
-    public static String GET_CUSTOMER = BASE_URL+"/v1/app/customer/getCustomer ";
+    public static String GET_CUSTOMER = BASE_URL+"/v1/app/customer/getCustomer";
+    public static String LOGIN_OUT = BASE_URL+"/v1/app/user/logout";
+    public static String CUSTOMER_INTENT=BASE_URL+"/v1/app/customer/customerIntent";
+    public static String DELETE_INTENT = BASE_URL+"/v1/app/customer/deleteIntent";
+    public static String DELETE_GOODS = BASE_URL+"/v1/app/customer/deleteIntentProduct";
 }
